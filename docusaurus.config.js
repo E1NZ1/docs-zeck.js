e2403e5880adedb959ed2fdc989692da99df936e
E1NZ1 <125573245+E1NZ1@users.noreply.github.com>
2023-03-13 18:24:18 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AgniDevelopment', // Usually your GitHub org/user name.
-  projectName: 'zeck.js', // Usually your repo name.
+  projectName: 'docs-zeck.js', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -36,9 +36,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/E1NZ1/docs-zeck.js',
-        },
+          editUrl: false,
         blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
