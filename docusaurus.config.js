4f0a5170e69ec7303a380b4a325874fef6308eb2
E1NZ1 <125573245+E1NZ1@users.noreply.github.com>
2023-03-13 20:06:22 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
