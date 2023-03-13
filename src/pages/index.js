398c98f24f7aa35e90b940da50885d8cd46f21f5
E1NZ1 <125573245+E1NZ1@users.noreply.github.com>
2023-03-13 18:19:06 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Zeck.js`}
+      description="LightWeight Npm package for discord.js<head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
